b39d551f46fdc4fdbaba9e34a3721c75b371cf71
Júlia Ilášová <114401595+juuwel@users.noreply.github.com>
2025-12-15 10:01:48 +0100

    Set up e2e (#5)
    * setup playwright and rename BFF to Frontend
    
    * add Docker configuration
    
    * adjust E2E action
    
    * ?
    
    * add --ignore-scripts flag to npm ci
    
    * add application startup to e2e pipeline and make a sample e2e test
    
    * fix docker build process through compose

--- Frontend/README.md
+++ Frontend/README.md
@@ -1,4 +1,4 @@
-# BFF
+# Box Factory Frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.2.
 
--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'BFF'`, () => {
+  it(`should have as title 'Box Factory'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('BFF');
+    expect(app.title).toEqual('Box Factory');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('BFF app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Box Factory app is running!');
   });
 });
