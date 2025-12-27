6d1414523a563587d262a5c90a506603d8289348
Júlia Ilášová <1julka1il@gmail.com>
2025-12-27 18:15:08 +0100

    try declaring navbar in unit  test

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
+  beforeEach(async () => await TestBed.configureTestingModule({
     imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+    declarations: [AppComponent, NavMenuComponent]
+  }).compileComponents()
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
